188dad209b76ef47dfc809fe79c3ae3f7c712599
Abrar Galib <galibabrar06@gmail.com>
2026-07-17 04:49:02 +0600

    Remove old background system

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -2,50 +2,21 @@ import { Outlet } from "react-router-dom";
 
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import AnimatedBackground from "../components/Background/AnimatedBackground";
-
 
 const RootLayout = () => {
-
   return (
-
     <div className="relative min-h-screen">
-
-
-      {/* Animated WebGL Background */}
-
-      <AnimatedBackground />
-
-
-
-      {/* Website Content */}
-
       <div className="relative z-10 flex min-h-screen flex-col">
-
-
         <Header />
 
-
-
-        <main className="relative flex-1 mx-auto w-full max-w-7xl px-6 py-8">
-
+        <main className="mx-auto w-full max-w-7xl flex-1 px-6 py-8">
           <Outlet />
-
         </main>
 
-
-
         <Footer />
-
-
       </div>
-
-
     </div>
-
   );
-
 };
-
 
 export default RootLayout;
